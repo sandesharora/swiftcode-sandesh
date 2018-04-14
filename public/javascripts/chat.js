b4ada970ab04b439b5dae4dcd5a735af8c73f951
sandesharora <sandesharora196@gmail.com>
2018-04-14 17:42:50 +0530

    alignment done

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,6 +1,20 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'hello'
+ 		},
+        {
+            'sender': 'BOT',
+            'text': 'how are you!!!'
+ 		},
+        {
+            'sender': 'USER',
+            'text': 'I am fine'
+ 		}
+ 		];
 
 
 });
